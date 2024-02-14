ad0b9eb63ca0a72c24060449457f9745781266b8
OrneysOriginal <email_address>
2024-02-14 18:23:56 +0300

    solution

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -1,9 +1,9 @@
 from catalog import views
+
 from django.urls import path
 
 
 urlpatterns = [
     path("catalog/", views.item_list),
     path("catalog/<int:pk>/", views.item_detail),
-    path("catalog/<int:pk>", views.item_detail),
 ]
