356fec7f1f289acfa789cc156725b8cf8d571678
Георгий Савин <georgijsavin17122@gmail.com>
2024-02-14 16:07:24 +0000

    Update urls.py

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path("catalog/", views.item_list),
-    path("catalog/<int:pk>/", views.item_detail),
+    path("catalog/<int:pk>", views.item_detail),
 ]
