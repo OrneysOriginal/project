52abe0d1bd801480f74c235116821ac30003b425
OrneysOriginal <email_address>
2024-02-14 21:25:33 +0300

    solution

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -40,10 +39,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-INTERNAL_IPS = ["127.0.0.1", "localhost"]
+if DEBUG:
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware",]
+    INTERNAL_IPS = ["127.0.0.1", "localhost"]
+    INSTALLED_APPS += ["debug_toolbar"]
 
 ROOT_URLCONF = "lyceum.urls"
 
