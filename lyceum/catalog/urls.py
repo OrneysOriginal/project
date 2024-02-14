c9474060115726d493ae7a6ceb27e843cbf0ed8d
OrneysOriginal <email_address>
2024-02-14 12:32:12 +0300

    l2task1

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from . import views
+from catalog import views
 
 
 urlpatterns = [
-    path("", views.item_list),
-    path("catalog/<int:pk>", views.item_detail),
+    path("catalog/", views.item_list),
+    path("catalog/<int:pk>/", views.item_detail),
 ]
