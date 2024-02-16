413d909d678893d7c80e8df6fd68a51960946acf
OrneysOriginal <email_address>
2024-02-16 16:06:43 +0300

    create middleware

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -10,15 +10,12 @@ class TestHomepage(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_teapot(self):
-        response = Client().get("/coffee/")
-        self.assertEqual(response.status_code, HTTPStatus.IM_A_TEAPOT)
-        self.assertEqual(response.content.decode(), "Я чайник")
-        lst = [0] * 10
-        for i in range(10):
-            lst[i] = Client().get("/coffee/")
+        lst = []
+        for i in range(9):
+            lst.append(Client().get("/coffee/"))
         for i in range(len(lst)):
             self.assertEqual(lst[i].status_code, HTTPStatus.IM_A_TEAPOT)
-            if i == 10:
+            if i == 0:
                 self.assertEqual(lst[i].content.decode(), "Я кинйач")
             else:
                 self.assertEqual(lst[i].content.decode(), "Я чайник")
