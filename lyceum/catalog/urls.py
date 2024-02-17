bc44c36ed11784fca02e9b85c84df9ea6aa4f968
OrneysOriginal <email_address>
2024-02-17 16:38:57 +0300

    solution

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -5,8 +5,8 @@ from django.urls import path, re_path, register_converter
 register_converter(converter.IntPLusDig, "value")
 
 urlpatterns = [
-    path("catalog/", views.item_list),
-    path("catalog/<int:pk>/", views.item_detail),
-    re_path(r"^catalog/re/(?P<pk>[1-9]\d*)/$", views.some_re),
-    path("catalog/converter/<value:pk>/", views.some_converter),
+    path("", views.item_list),
+    path("<int:pk>/", views.item_detail),
+    re_path(r"^re/(?P<pk>[1-9]\d*)/$", views.some_re),
+    path("converter/<value:pk>/", views.some_converter),
 ]
--- lyceum/homepage/urls.py
+++ lyceum/homepage/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 from homepage import views
 
@@ -6,5 +5,4 @@ from homepage import views
 urlpatterns = [
     path("", views.homepage),
     path("coffee/", views.teapot),
-    path("admin/", admin.site.urls),
 ]
