3a5e21a266eb7f73d36592f018793284831097cd
OrneysOriginal <email_address>
2024-03-02 01:25:38 +0300

    solution

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from lyceum import settings
+from django.conf import settings
 
 
 urlpatterns = [
@@ -13,7 +13,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+    urlpatterns += (path("__debug__/", include("debug_toolbar.urls")),)
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT,
