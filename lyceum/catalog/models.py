d6a0c9e11f9f4fd2e72c62ff948d597533f7a265
OrneysOriginal <email_address>
2024-03-06 23:04:57 +0300

    solution

--- lyceum/catalog/models.py
+++ lyceum/catalog/models.py
@@ -96,6 +96,7 @@ class Item(AbstractCatalog):
         verbose_name="категории",
         unique=False,
         help_text="Выберите категорию",
+        related_name="item"
     )
     tags = django.db.models.ManyToManyField(
         Tag,
