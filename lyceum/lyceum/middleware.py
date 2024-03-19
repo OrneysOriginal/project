1888258a950b41fbbf098af49bb5dcf36348088a
OrneysOriginal <email_address>
2024-03-19 17:47:50 +0300

    fixed middleware

--- lyceum/lyceum/middleware.py
+++ lyceum/lyceum/middleware.py
@@ -1,10 +1,10 @@
 import re
 
-from django.conf import settings
-from django.core import mail
-from django.core.mail import send_mail
+import django.conf
 
-from lyceum.settings import DJANGO_MAIL
+
+WORDS_REGEX = re.compile(r"\w+|\W+")
+NOT_RUSSIAN_REGEX = re.compile(r"^[^а-яА-Я\s]+$")
 
 
 class ReverseRusWordMiddleware:
@@ -14,41 +14,31 @@ class ReverseRusWordMiddleware:
         self.get_response = get_response
 
     @classmethod
-    def change_count(cls):
-        if cls.count == 10:
-            cls.count = 1
-        else:
-            cls.count += 1
+    def check_need_reverse(cls):
+        if not django.conf.settings.ALLOW_REVERSE:
+            return False
+
+        cls.count += 1
+        if cls.count != 10:
+            return False
+
+        cls.count = 0
+        return True
 
     def __call__(self, request):
+        if not self.check_need_reverse():
+            return self.get_response(request)
+
         response = self.get_response(request)
-        if ReverseRusWordMiddleware.count == 10:
-            if request.method == "POST":
-                text = request.POST.get("text")[::-1]
-                send_mail(
-                    "",
-                    text,
-                    f"from{DJANGO_MAIL}",
-                    [mail],
-                    fail_silently=False,
-                )
+        content = response.content.decode()
+        words = WORDS_REGEX.findall(content)
 
-            if settings.ALLOW_REVERSE:
-                text = response.content.decode("utf-8")
-                words = re.findall(r"\b[а-яА-ЯёЁ]+\b", text)
-                rus_reverse_words = {}
-                for i in range(len(words)):
-                    rus_reverse_words[words[i]] = words[i][::-1]
+        transformed = [
+            word if NOT_RUSSIAN_REGEX.search(word) else word[::-1]
+            for word in words
+        ]
 
-                strip_text = text.strip("<body>").strip("</>").split()
-                for i in range(len(strip_text)):
-                    if rus_reverse_words.get(strip_text[i]) is not None:
-                        strip_text[i] = rus_reverse_words[strip_text[i]]
-
-                text = " ".join(strip_text)
-                response.content = text.encode("utf-8")
-
-        self.change_count()
+        response.content = "".join(transformed).encode()
         return response
 
 
